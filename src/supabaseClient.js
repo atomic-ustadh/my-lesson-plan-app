4a15bee24cb3079a2f40d7466b9a3e550aab766c
Abdurrahman Luqman Yusuf <110829892+atomic-ustadh@users.noreply.github.com>
2026-01-25 03:21:42 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,4 +4,10 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true, // Keep the session alive in local storage
+    autoRefreshToken: true, // Automatically refresh the session token
+    detectSessionInUrl: true, // Crucial for parsing sessions from URL hash fragments
+  },
+})
