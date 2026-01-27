653e96a7b536efbcc2db17e9689ec78ec1ebb62d
Abdurrahman Luqman Yusuf <110829892+atomic-ustadh@users.noreply.github.com>
2026-01-27 19:37:51 +0100

    SiteMap & Vite Sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
+
+const dynamicRoutes = [
+  '/about',
+  '/privacy-policy',
+  '/terms-of-use',
+  '/login'
+]
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://my-lesson-plan-app.netlify.app',
+      dynamicRoutes
+    })
+  ],
 })
